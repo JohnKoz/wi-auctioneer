f906caac086631ac5033a1f3036feab3a40d9bda
Nick Heidke <nick.heidke@gmail.com>
2016-12-29 18:35:42 -0600

    Adding web project, woot!

---
+++ surplus-auctioneer-webapp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace surplus_auctioneer_webapp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
